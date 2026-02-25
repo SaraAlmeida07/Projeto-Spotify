9400f35d16dab26a5ae226aa5c19044a19350e07
SaraAlmeida07 <saraabner07@gmail.com>
2026-02-25 11:27:24 -0300

    desenhando o btn de like

--- script.js
+++ script.js
@@ -11,36 +11,43 @@ const shuffleButton = document.getElementById("shuffle");
 const repeatButton = document.getElementById("repeat");
 const songTime = document.getElementById("song-time");
 const totalTime = document.getElementById("total-time");
-
-
+const btnLike = document.getElementById("like");
 
 const naci = {
   songName: "Naci Otra Vez",
   artist: "J Arias",
   file: "naciOtraVez",
+  liked: true,
 };
 
 const colors = {
   songName: "Colors",
   artist: "Black Pumas",
   file: "colors",
+  liked: false,
+};
+
+const npp = {
+  songName: "N'y Pense Plus",
+  artist: "Tayc",
+  file: "N'y Pense Plus",
+  liked: false,
 };
 
 const tadow = {
   songName: "Tadow",
   artist: "Massego",
   file: "tadow",
+  liked: false,
 };
 
 let isplaying = false;
 let isShuffled = false;
-const originalPlaylist = [naci, colors, tadow];
+const originalPlaylist = [naci, colors, npp, tadow];
 let index = 0;
 let sortedPlaylist = [...originalPlaylist];
 
 let repeatOn = false;
-
-
 
 function playSong() {
   play.querySelector(".bi").classList.remove("bi-play-circle-fill");
@@ -71,58 +78,52 @@ function initializeSong() {
   song.src = `songs/${sortedPlaylist[index].file}.mp3`;
   songName.innerText = sortedPlaylist[index].songName;
   bandName.innerText = sortedPlaylist[index].artist;
+  likeButtonRender();
 }
 
 function previousSong() {
-    if(index === 0) {
-        index = sortedPlaylist.length -1;
-    }
-    else
-    index -= 1;
-    initializeSong();
-    playSong();
+  if (index === 0) {
+    index = sortedPlaylist.length - 1;
+  } else index -= 1;
+  initializeSong();
+  playSong();
 }
 
 function nextSong() {
-    if(index === sortedPlaylist.length -1) {
-        index = 0;
-    }
-    else
-    index += 1;
-    initializeSong();
-    playSong();
+  if (index === sortedPlaylist.length - 1) {
+    index = 0;
+  } else index += 1;
+  initializeSong();
+  playSong();
 }
 
 function updateProgress() {
-  
-    const barWidth = (song.currentTime/ song.duration)*100 ;
-    currentProgress.style.setProperty("--progress", `${barWidth}%`);
-    totalTime.innerText = toTHHMMSS(song.currentTime);
+  const barWidth = (song.currentTime / song.duration) * 100;
+  currentProgress.style.setProperty("--progress", `${barWidth}%`);
+  totalTime.innerText = toTHHMMSS(song.currentTime);
 }
 
 function jumpTo(event) {
-    const width = progressContainer.clientWidth;
-    const clickPosition = event.offsetX;
-    const jumpToTime = (clickPosition/width)* song.duration;
-    song.currentTime = jumpToTime;
+  const width = progressContainer.clientWidth;
+  const clickPosition = event.offsetX;
+  const jumpToTime = (clickPosition / width) * song.duration;
+  song.currentTime = jumpToTime;
 }
 
 function shuffleArray(preshuffleArray) {
-  let currentIndex = preshuffleArray.length -1; 
+  let currentIndex = preshuffleArray.length - 1;
 
   while (currentIndex > 0) {
-    let randomIndex = Math.floor(Math.random()* (currentIndex +1));
+    let randomIndex = Math.floor(Math.random() * (currentIndex + 1));
     let aux = preshuffleArray[currentIndex];
     preshuffleArray[currentIndex] = preshuffleArray[randomIndex];
     preshuffleArray[randomIndex] = aux;
 
-    currentIndex-= 1;
-
+    currentIndex -= 1;
   }
-
 }
 function shuffleButonClick() {
-  if(isShuffled === false) {
+  if (isShuffled === false) {
     isShuffled = true;
     shuffleArray(sortedPlaylist);
     shuffleButton.classList.add("button-active");
@@ -133,59 +134,62 @@ function shuffleButonClick() {
     index = 0;
     initializeSong();
     playSong();
-
   }
-
 }
 
-function repeatButtonClick(){
-  if(repeatOn === false){
+function repeatButtonClick() {
+  if (repeatOn === false) {
     repeatOn = true;
     repeatButton.classList.add("button-active");
-
   } else {
     repeatOn = false;
     repeatButton.classList.remove("button-active");
-
   }
-
 }
 
-function nextOrRepeat(){
-  if(repeatOn === false) {
+function nextOrRepeat() {
+  if (repeatOn === false) {
     nextSong();
-  }
-    
-  else {
+  } else {
     playSong();
   }
 }
 
-function toTHHMMSS(originalNumber){
-  let hours = Math.floor(originalNumber/3600);
-  let min = Math.floor((originalNumber - hours * 3600) / 60) ;
-  let segs = Math.floor(originalNumber - hours * 3600 - min *60);
+function toTHHMMSS(originalNumber) {
+  let hours = Math.floor(originalNumber / 3600);
+  let min = Math.floor((originalNumber - hours * 3600) / 60);
+  let segs = Math.floor(originalNumber - hours * 3600 - min * 60);
 
-  return `${hours.toString().padStart(2, "0")}:${min.toString().padStart(2,"0")}:${segs.toString().padStart(2,"0")}`;
+  return `${hours.toString().padStart(2, "0")}:${min.toString().padStart(2, "0")}:${segs.toString().padStart(2, "0")}`;
 }
 
 function updateTotalTime() {
-
   songTime.innerText = toTHHMMSS(song.duration);
-  
 }
 
+function likeButtonRender() {
+  if (sortedPlaylist[index].liked) {
+    btnLike.querySelector(".bi").classList.remove("bi-heart");
+    btnLike.querySelector(".bi").classList.add("bi-heart-fill");
+    btnLike.classList.add("button-active");
+  } else {
+    btnLike.querySelector(".bi").classList.add("bi-heart");
+    btnLike.querySelector(".bi").classList.remove("bi-heart-fill");
+    btnLike.classList.remove("button-active");
+  }
+}
 
-
+function likeButtonClick() {}
 
 initializeSong();
 
 play.addEventListener("click", playPauseDecider);
 previous.addEventListener("click", previousSong);
-pass.addEventListener("click",nextSong);
+pass.addEventListener("click", nextSong);
 song.addEventListener("timeupdate", updateProgress);
 song.addEventListener("ended", nextOrRepeat);
 song.addEventListener("loadedmetadata", updateTotalTime);
 progressContainer.addEventListener("click", jumpTo);
 shuffleButton.addEventListener("click", shuffleButonClick);
-repeatButton.addEventListener("click", repeatButtonClick)
+repeatButton.addEventListener("click", repeatButtonClick);
+btnLike.addEventListener("click", likeButtonClick);
